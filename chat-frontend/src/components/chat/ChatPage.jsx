6017205c920b70d6380f41757b334057df24940c
Aman Shakya <aman.shakya@analyticsvidhya.com>
2023-06-01 13:00:13 +0530

    emoji icon changed

--- chat-frontend/src/components/chat/ChatPage.jsx
+++ chat-frontend/src/components/chat/ChatPage.jsx
@@ -76,7 +76,7 @@ export default function ChatPage({submitMessage,message,user,currentuser,callUse
       </div>
       <form className='py-3' onSubmit={(e)=>{submitMessage(e,chatId)}}>
         <div className="emoji-wrapper mx-3">
-          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM176.4 176a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm128 32a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zM256 288a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
+          <img src="/images/emoji-icon.gif" alt="emoji icon" width={40}  />
           <div className="emoji-box d-flex flex-wrap shadow p-2 justify-content-center">
             <span>😀</span>
             <span>😃</span>
